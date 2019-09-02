4e12161335ed84f975541a4c5499b52972706812
guyunxiang <yxgu@sparkpool.com>
2019-09-02 18:36:50 +0800

    init commit

---
+++ example/src/pages/index.js
@@ -0,0 +1,11 @@
+import ExampleComponent from '{{name}}';
+
+import styles from './index.css';
+
+export default function() {
+  return (
+    <div className={styles.normal}>
+      <ExampleComponent />
+    </div>
+  );
+}
